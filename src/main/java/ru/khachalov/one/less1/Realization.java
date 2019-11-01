f24cbc2727a9bf157c4db0f265fcdb8fcec92826
omax3912@gmail.com <omax@WIN-P676IIU2OFG.localdomain>
2019-11-01 18:34:10 +0300

    del lishnee

--- src/main/java/ru/khachalov/one/less1/Realization.java
+++ src/main/java/ru/khachalov/one/less1/Realization.java
@@ -1,19 +1,34 @@
 package ru.khachalov.one.less1;
 
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Realization implements FizzBuzz {
     static String result = "";
+    ArrayList resul = new ArrayList();
     @Override
     public String print(int from, int to) {
         if (to >= from & from >= 0) {
             for (int i = from; i <= to; i++) {
                 if (i % 3 == 0 & i % 5 == 0) {
                     result = "FizzBuzz";
+                    resul.add(result + "(" + i + ")");
                 } else if (i % 5 == 0) {
                     result = "Buzz";
+                    resul.add(result + "(" + i + ")");
                 } else if (i % 3 == 0) {
                     result = "Fizz";
+                    resul.add(result + "(" + i + ")");
                 } else {
                     result = String.valueOf(i);
+                    resul.add(result);
+                }
+                //System.out.print(resul.get(i - 1) + "   ");
+                //OutputOnDisplay.io.print(resul.get(i - 1) + "   ");
+                if (i % 15 == 0){
+                    //System.out.println();
+                    //OutputOnDisplay.io.println();
                 }
             }
         } else {
@@ -23,6 +38,6 @@ public class Realization implements FizzBuzz {
     }
 
     public static void main(String[] args) {
-        new Realization().print(1, 1000);
+        new Realization().print(1,100);
     }
 }
