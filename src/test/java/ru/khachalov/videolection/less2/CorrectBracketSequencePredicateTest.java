2f7c49e58fab3733201781a41e1c6e4141dc1c8c
omax39 <omax3912@gmail.com>
2019-11-27 17:28:31 +0300

    three homeworks comlete less easysorts

---
+++ src/test/java/ru/khachalov/videolection/less2/CorrectBracketSequencePredicateTest.java
@@ -0,0 +1,54 @@
+package ru.khachalov.videolection.less2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CorrectBracketSequencePredicateTest {
+
+    @Test
+    void nullString() {
+        assertEquals(true,
+                CorrectBracketSequencePredicate.test(""));
+    }
+
+    @Test
+    void correctString(){
+        assertEquals(true,
+                CorrectBracketSequencePredicate.test("(())(())"));
+        assertEquals(true,
+                CorrectBracketSequencePredicate.test("{(())()}"));
+        assertEquals(true,
+                CorrectBracketSequencePredicate.test("(())()"));
+        assertEquals(true,
+                CorrectBracketSequencePredicate.test("{(())()[{}]}"));
+        assertEquals(true,
+                CorrectBracketSequencePredicate.test("{(())()}[{}]()"));
+        assertEquals(true,
+                CorrectBracketSequencePredicate.test("{(()){{}}()}"));
+        assertEquals(true,
+                CorrectBracketSequencePredicate.test("{(({}))()}"));
+        assertEquals(true,
+                CorrectBracketSequencePredicate.test("()"));
+        assertEquals(true,
+                CorrectBracketSequencePredicate.test("([])"));
+        assertEquals(true,
+                CorrectBracketSequencePredicate.test("()[]"));
+        assertEquals(true,
+                CorrectBracketSequencePredicate.test("{()}"));
+    }
+
+    @Test
+    void incorrectString(){
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+            CorrectBracketSequencePredicate.test(")(");
+            CorrectBracketSequencePredicate.test("][");
+            CorrectBracketSequencePredicate.test("}{");
+            CorrectBracketSequencePredicate.test("(}");
+            CorrectBracketSequencePredicate.test("{]");
+            CorrectBracketSequencePredicate.test("{{{]]");
+            CorrectBracketSequencePredicate.test("{([)}");
+                });
+    }
+}
