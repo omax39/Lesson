20e9a21e4225255aeb3cb3bbc3a4ccd3f465e39b
omax39 <omax3912@gmail.com>
2019-12-04 10:12:13 +0300

    add finders for Library

--- src/main/java/ru/khachalov/biblioteka/Library.java
+++ src/main/java/ru/khachalov/biblioteka/Library.java
@@ -14,7 +14,43 @@ public class Library {
         books.get(index);
 
     }
-    public void deleteBook(){
-
+    public void deleteBook(int index){
+        books.remove(index);
+    }
+    public int findOfName(String name){
+        int i;
+        for (i = 0; i < books.size(); i++){
+            if (books.get(i).getName().equals(name)){
+                break;
+            }
+        }
+        return i;
+    }
+    public int findOfPlace(String place){
+        int i;
+        for (i = 0; i < books.size(); i++){
+            if (books.get(i).getPlace().equals(place)){
+                return i;
+            }
+        }
+        return i;
+    }
+    public List findOfSection(String section){
+        List<Integer> booksOfSection = new ArrayList<>();
+        for (int i = 0; i < books.size(); i++){
+            if (books.get(i).getSection().equals(section)){
+                booksOfSection.add(i);
+            }
+        }
+        return booksOfSection;
+    }
+    public List findOfId(int id){
+        List<Integer> booksOfId = new ArrayList<>();
+        for (int i = 0; i < books.size(); i++){
+            if (books.get(i).getId()==id){
+                booksOfId.add(i);
+            }
+        }
+        return booksOfId;
     }
 }
